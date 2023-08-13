9b3c047b34037954dd21d650f8058fbf89954ac2
Michael Hardy <mhar860@aucklanduni.ac.nz>
2023-08-14 11:13:33 +1200

    feat: implemented create event activity and .xml file intergrated with Fragment

--- app/src/main/java/com/example/a310_rondayview/MainActivity.java
+++ app/src/main/java/com/example/a310_rondayview/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.a310_rondayview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.a310_rondayview.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new CreateEventFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.create) {
+                replaceFragment(new CreateEventFragment());
+            }
+
+            return true;
+        });
+
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
