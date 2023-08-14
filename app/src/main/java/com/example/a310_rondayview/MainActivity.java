419d1bbfd2494a895b3f31b83e9380b0a68c4490
Devesh Duptala <dduptala@gmail.com>
2023-08-14 18:43:47 +1200

    Cleaned up main activity a bit

--- app/src/main/java/com/example/a310_rondayview/MainActivity.java
+++ app/src/main/java/com/example/a310_rondayview/MainActivity.java
@@ -15,51 +15,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
-//public class MainActivity extends AppCompatActivity {
-//
-//    private static final String TAG = "FirestoreTest";
-//    FirebaseFirestore firestore;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        firestore = FirebaseFirestore.getInstance();
-//
-//        // Replace "yourDocumentId" with the actual document ID you want to retrieve
-//        String documentId = "JrffZAxQzSWQSvnMMKYG";
-//
-//        retrieveAndPrintDocumentData(documentId);
-//    }
-//
-//    private void retrieveAndPrintDocumentData(String documentId) {
-//        firestore.collection("events").document(documentId).get()
-//                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//                    @Override
-//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                        if (documentSnapshot.exists()) {
-//                            // Retrieve data from the document
-//                            String eventName = documentSnapshot.getString("eventName");
-//                            String eventDate = documentSnapshot.getString("eventDate");
-//
-//                            // Print the retrieved data to the console
-//                            Log.d(TAG, "Event Name: " + eventName);
-//                            Log.d(TAG, "Event Date: " + eventDate);
-//                        } else {
-//                            Log.d(TAG, "Document does not exist");
-//                        }
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.e(TAG, "Error getting document", e);
-//                    }
-//                });
-//    }
-//}
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "FirestoreTest";
@@ -70,12 +25,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // example
         firestore = FirebaseFirestore.getInstance();
 
         // Data to be added to the new document
         Map<String, Object> newEventData = new HashMap<>();
-        newEventData.put("eventName", "New Event");
-        newEventData.put("eventDate", "August 20");
+        newEventData.put("eventName", "Dummy Test");
+        newEventData.put("eventDate", "Test Date");
 
         addNewEvent(newEventData);
     }
