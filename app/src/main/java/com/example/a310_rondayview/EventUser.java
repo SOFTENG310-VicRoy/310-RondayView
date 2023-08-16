5d5b4c5df16db8fe1b82591192f7aca283d2c952
Jackson Schofield <jacksonschofield@wifi-172-23-122-218.uoa-wifi.auckland.ac.nz>
2023-08-16 12:49:42 +1200

    added cllas diagram classes

---
+++ app/src/main/java/com/example/a310_rondayview/EventUser.java
@@ -0,0 +1,12 @@
+package com.example.a310_rondayview;
+
+import java.util.List;
+
+public class EventUser extends User{
+    List<Event> likedEvents;
+
+    void likeEvent(){}
+    void unLikeEvent(){}
+    void viewEvent(){}
+
+}
---
+++ app/src/main/java/com/example/a310_rondayview/User.java
@@ -0,0 +1,7 @@
+package com.example.a310_rondayview;
+
+public abstract class User {
+    FireBaseUser fireBaseUser;
+    String username;
+    void updateProfile(){}
+}
