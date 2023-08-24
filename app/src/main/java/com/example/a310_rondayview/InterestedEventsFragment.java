2eb2b4bcee80700e9d9e6646a06c6e08f03c917c
arielyxw <wany468@aucklanduni.ac.nz>
2023-08-24 14:52:31 +1200

    Cleaned up code

--- app/src/main/java/com/example/a310_rondayview/InterestedEventsFragment.java
+++ app/src/main/java/com/example/a310_rondayview/InterestedEventsFragment.java
@@ -11,23 +11,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.a310_rondayview.Event;
 import com.example.a310_rondayview.ui.InterestedEventsAdapter;
-import com.google.firebase.Timestamp;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class InterestedEventsFragment extends Fragment {
 
     // the fragment initialization parameters
     private ArrayList<Event> eventsArrayList;
-
-    private int[] eventsImages;
-    private String[] eventsTitles;
-    private String[] eventsDescriptions;
-    private RecyclerView recyclerview;
-
 
     public InterestedEventsFragment() {
         // Required empty public constructor
@@ -42,36 +33,36 @@ public class InterestedEventsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // setup the recycler view
         super.onViewCreated(view, savedInstanceState);
         dataInitialise();
-        recyclerview = view.findViewById(R.id.recyclerView);
+        RecyclerView recyclerview = view.findViewById(R.id.recyclerView);
         recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerview.setHasFixedSize(true);
 
-        Event fakeEvent1 = new Event("fakeId1", "WDCC", "Fake Event1", "Web dev club", "UoA", new Timestamp(new Date()), "https://firebasestorage.googleapis.com/v0/b/rondayview-872b4.appspot.com/o/eventImages%2FeventSpeedInterviews.png?alt=media&token=0d878d83-1308-487f-87f4-386f41a99e5f", "https://firebasestorage.googleapis.com/v0/b/rondayview-872b4.appspot.com/o/eventImages%2FSESA.png?alt=media&token=d16dda80-cc0b-4ea6-a4bb-4f9a02fc9fe4");
-        eventsArrayList.add(fakeEvent1);
-        Event fakeEvent2 = new Event("fakeId2", "SESA","Fake Event2", "Software engineering student association", "UoA", new Timestamp(new Date()), "https://firebasestorage.googleapis.com/v0/b/rondayview-872b4.appspot.com/o/eventImages%2FeventSpeedInterviews.png?alt=media&token=0d878d83-1308-487f-87f4-386f41a99e5f", "https://firebasestorage.googleapis.com/v0/b/rondayview-872b4.appspot.com/o/eventImages%2FSESA.png?alt=media&token=d16dda80-cc0b-4ea6-a4bb-4f9a02fc9fe4");
-        eventsArrayList.add(fakeEvent2);
-
+        // populate the recycler view
         InterestedEventsAdapter interestedEventsAdapter = new InterestedEventsAdapter(getContext(), eventsArrayList);
         recyclerview.setAdapter(interestedEventsAdapter);
         interestedEventsAdapter.notifyDataSetChanged();
     }
 
+    //ToDo Implement populating the favourites list with database events
     private void dataInitialise() {
+        // get events from the database
         eventsArrayList = new ArrayList<>();
-        eventsImages = new int[] {
+        int[] eventsImages = new int[] {
                 // get list of images
         };
-        eventsTitles = new String[] {
+        // get list of titles
+        String[] eventsTitles = new String[]{
                 // get list of titles
         };
-        eventsDescriptions = new String[] {
+        String[] eventsDescriptions = new String[] {
                 // get list of descriptions
         };
 
+        // add events to the favourites list
         for (int i = 0; i < eventsTitles.length; i++) {
-
             Event event = new Event(); // populate each event with fields from corresponding indices in the arrays
             eventsArrayList.add(event);
         }
--- app/src/main/java/com/example/a310_rondayview/ui/InterestedEventsAdapter.java
+++ app/src/main/java/com/example/a310_rondayview/ui/InterestedEventsAdapter.java
@@ -1,7 +1,6 @@
 package com.example.a310_rondayview.ui;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
