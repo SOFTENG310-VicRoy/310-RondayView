da4fa9587ba6bdad11498eb259f8c6364bd4c795
arielyxw <wany468@aucklanduni.ac.nz>
2023-08-24 15:03:11 +1200

    More code clean up

--- app/src/main/java/com/example/a310_rondayview/InterestedEventsFragment.java
+++ app/src/main/java/com/example/a310_rondayview/InterestedEventsFragment.java
@@ -33,9 +33,11 @@ public class InterestedEventsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        // setup the recycler view
         super.onViewCreated(view, savedInstanceState);
-        dataInitialise();
+
+        // load in the list of favourited events from there database here
+
+        // setup the recycler view
         RecyclerView recyclerview = view.findViewById(R.id.recyclerView);
         recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerview.setHasFixedSize(true);
@@ -44,27 +46,5 @@ public class InterestedEventsFragment extends Fragment {
         InterestedEventsAdapter interestedEventsAdapter = new InterestedEventsAdapter(getContext(), eventsArrayList);
         recyclerview.setAdapter(interestedEventsAdapter);
         interestedEventsAdapter.notifyDataSetChanged();
-    }
-
-    //ToDo Implement populating the favourites list with database events
-    private void dataInitialise() {
-        // get events from the database
-        eventsArrayList = new ArrayList<>();
-        int[] eventsImages = new int[] {
-                // get list of images
-        };
-        // get list of titles
-        String[] eventsTitles = new String[]{
-                // get list of titles
-        };
-        String[] eventsDescriptions = new String[] {
-                // get list of descriptions
-        };
-
-        // add events to the favourites list
-        for (int i = 0; i < eventsTitles.length; i++) {
-            Event event = new Event(); // populate each event with fields from corresponding indices in the arrays
-            eventsArrayList.add(event);
-        }
     }
 }
