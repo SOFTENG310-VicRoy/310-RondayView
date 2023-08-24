73d062aea47e4ba3e40ad05aeee5836cc79d86f3
Jackson Schofield <jacksonschofield@Jacksons-MacBook-Pro.local>
2023-08-24 16:58:02 +1200

    added auth

--- app/src/main/java/com/example/a310_rondayview/FragmentAccount.java
+++ app/src/main/java/com/example/a310_rondayview/FragmentAccount.java
@@ -1,20 +1,41 @@
 package com.example.a310_rondayview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FragmentAccount extends Fragment {
 
-    public FragmentAccount() {
-        // Required empty public constructor
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        // Sign out button
+        view.findViewById(R.id.btnSignOut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                // Navigate back to Sign-In screen
+                Intent signInIntent = new Intent(getActivity(), SignInActivity.class);
+                startActivity(signInIntent);
+
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/a310_rondayview/MainActivity.java
+++ app/src/main/java/com/example/a310_rondayview/MainActivity.java
@@ -1,43 +1,52 @@
 package com.example.a310_rondayview;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.a310_rondayview.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import com.example.a310_rondayview.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     private static final String TAG = "FirestoreTest";
     private FirebaseFirestore firestore;
+    private FirebaseAuth mAuth;
 
     // TODO ("replaceFragment(new CreateEventFragment());") replace CreateEventFragment with
     // the homepage once the homepage is made
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        firestore = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            // User is not signed in, redirect to login
+            startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            finish(); // Prevents users from coming back to this activity when back button is pressed
+            return;
+        }
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         replaceFragment(new FragmentHome());
         ImageButton heartButton = findViewById(R.id.heartButton);
         heartButton.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +68,16 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
+        // example
+        firestore = FirebaseFirestore.getInstance();
+
+        // Data to be added to the new document
+        Map<String, Object> newEventData = new HashMap<>();
+        newEventData.put("eventName", "Dummy Test");
+        newEventData.put("eventDate", "Test Date");
+
+        addNewEvent(newEventData);
+
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -66,5 +85,21 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    private void addNewEvent(Map<String, Object> eventData) {
+        firestore.collection("events").add(eventData)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "New event added successfully");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Error adding new event", e);
+                    }
+                });
     }
 }
