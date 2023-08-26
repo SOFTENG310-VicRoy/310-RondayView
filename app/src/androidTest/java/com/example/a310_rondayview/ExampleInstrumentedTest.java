f67c3a08f8ca271c859cbfa1e4353d2d7074d358
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-08-26 21:59:16 +1200

    Swipe feature (#58)
    * Added swipe functionality with Koloda
    
    * Added extra swipe functionality + minor design changes
    
    * Really basic test cases to see if the basic fragment is working
    
    * Instrument test fix, works now
    
    * removed Mokito test case dependancy
    
    * removed package mockito
    
    * remove mockito attempt

--- app/src/androidTest/java/com/example/a310_rondayview/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/a310_rondayview/ExampleInstrumentedTest.java
@@ -1,14 +1,24 @@
 package com.example.a310_rondayview;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
+import static java.util.regex.Pattern.matches;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,5 +32,19 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.a310_rondayview", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testNopeButton() {
+        // Perform a click on the nopeButton
+        onView(withId(R.id.nopeButton)).perform(click());
+
+        // respective views disabled/enabled
+        onView(withId(R.id.koloda)).check(matches(isDisplayed()));
+        onView(withId(R.id.buttonsContainer)).check(matches(isDisplayed()));
+        onView(withId(R.id.emptyEventsLayout)).check(matches(not(isDisplayed())));
     }
 }
---
+++ app/src/test/java/com/example/a310_rondayview/SwipeAdapterTest.java
@@ -0,0 +1,35 @@
+package com.example.a310_rondayview;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwipeAdapterTest {
+
+    @Test
+    public void testGetCount() {
+        List<Event> events = new ArrayList<>();
+        events.add(new Event());
+        events.add(new Event());
+        SwipeAdapter adapter = new SwipeAdapter(null, events);
+
+        assertEquals(2, adapter.getCount());
+    }
+
+    @Test
+    public void testGetItem() {
+        Event event = new Event();
+        List<Event> events = new ArrayList<>();
+        events.add(event);
+        SwipeAdapter adapter = new SwipeAdapter(null, events);
+        assertEquals(event, adapter.getItem(0));
+    }
+
+    @Test
+    public void testGetItemId() {
+        SwipeAdapter adapter = new SwipeAdapter(null, new ArrayList<>());
+        assertEquals(0, adapter.getItemId(0));
+    }
+}
