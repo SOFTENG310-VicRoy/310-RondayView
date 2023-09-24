35aae3451e420f92b9aabbefcc616a0fe7dd69f1
tmur647 <tmur647@aucklanduni.ac.nz>
2023-09-24 15:12:58 +1300

    add fragement

--- app/src/main/java/com/example/a310_rondayview/ui/detailed/FragmentDetailed.java
+++ app/src/main/java/com/example/a310_rondayview/ui/detailed/FragmentDetailed.java
@@ -2,6 +2,13 @@ package com.example.a310_rondayview.ui.detailed;
 
 import android.app.Fragment;
 
+
 public class FragmentDetailed extends Fragment {
+    public FragmentDetailed() {
+        // Required empty public constructor
+    }
+
+
+
 
 }
