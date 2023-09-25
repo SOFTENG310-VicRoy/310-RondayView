d42dad7c0bae3f43581e77fd6949178d0fee9b08
tmur647 <tmur647@aucklanduni.ac.nz>
2023-09-25 13:59:37 +1300

    add event to detailed page

--- app/src/main/java/com/example/a310_rondayview/ui/detailed/FragmentDetailed.java
+++ app/src/main/java/com/example/a310_rondayview/ui/detailed/FragmentDetailed.java
@@ -5,27 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.a310_rondayview.R;
-import com.example.a310_rondayview.ui.interestedevents.InterestedEventsFragment;
+import com.example.a310_rondayview.model.CurrentEvent;
 
 
 public class FragmentDetailed extends Fragment {
+    CurrentEvent currentEvent;
+    ViewHolder vh;
+
     public FragmentDetailed() {
         // Required empty public constructor
     }
     private class ViewHolder {
-
+        ImageView eventImage;
         public ViewHolder(View view) {
-
+            eventImage = view.findViewById(R.id.event_image);
         }
     }
 
@@ -34,7 +34,10 @@ public class FragmentDetailed extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detailed, container, false);
+        vh = new ViewHolder(view);
+        currentEvent = CurrentEvent.getInstance();
 
+        Glide.with(getContext()).load(currentEvent.getCurrentEvent().getImageURL()).into(vh.eventImage);
 
 
         return view;
