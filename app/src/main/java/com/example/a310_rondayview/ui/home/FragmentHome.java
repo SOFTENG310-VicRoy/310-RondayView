6304fc43339845b6552a407d44fcfff9e783bb3b
nbel806 <100734619+nbel806@users.noreply.github.com>
2023-09-27 12:26:35 +1300

    Refactor/refactor home (#89)
    * Rewrote Fragment home and moved data accessing to another class
    
    * Cleaned up
    
    * Delete app/src/test/java/com/example/a310_rondayview/UserDataManagerTest.java
    
    * Updated to run some tests
    
    * Resolved some sonar cloud bugs

--- app/src/main/java/com/example/a310_rondayview/ui/home/FragmentHome.java
+++ app/src/main/java/com/example/a310_rondayview/ui/home/FragmentHome.java
@@ -1,7 +1,6 @@
 package com.example.a310_rondayview.ui.home;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +13,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.a310_rondayview.R;
-import com.example.a310_rondayview.data.event.EventsFirestoreManager;
+import com.example.a310_rondayview.data.event.DatabaseService;
 import com.example.a310_rondayview.data.user.FireBaseUserDataManager;
 import com.example.a310_rondayview.model.Event;
 import com.example.a310_rondayview.ui.adapter.SwipeAdapter;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.yalantis.library.Koloda;
 import com.yalantis.library.KolodaListener;
 
@@ -29,31 +25,56 @@ import java.util.List;
 
 public class FragmentHome extends Fragment {
 
+    private static class ViewHolder {
+
+        LinearLayout buttonContainer;
+        LinearLayout emptyEventsLayout;
+
+        Button nopeButton;
+        Button interestedButton;
+        Button refreshButton;
+
+        Koloda koloda;
+
+        public ViewHolder(View rootView) {
+            // setting up views
+            buttonContainer = rootView.findViewById(R.id.buttonsContainer);
+            emptyEventsLayout = rootView.findViewById(R.id.emptyEventsLayout);
+            // Set up button click listeners
+            nopeButton = rootView.findViewById(R.id.nopeButton);
+            interestedButton = rootView.findViewById(R.id.interestedButton);
+            refreshButton = rootView.findViewById(R.id.refreshButton);
+            // setting up koloda (for the card swipes) - READ MORE HERE: https://github.com/Yalantis/Koloda-Android
+            koloda = rootView.findViewById(R.id.koloda);
+        }
+
+    }
+
     private static final String TAG = "FragmentHome";
     private SwipeAdapter adapter;
     private List<Event> events = new ArrayList<>();
-    private List<Event> disinterestedEvents = new ArrayList<>();
-    private int currentEventIndex = 0;
-    private LinearLayout buttonContainer;
-    private LinearLayout emptyEventsLayout;
-    Koloda koloda;
+    private int currentEventIndex;
+    private ViewHolder vh;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        // setting up views
-        buttonContainer = rootView.findViewById(R.id.buttonsContainer);
-        emptyEventsLayout = rootView.findViewById(R.id.emptyEventsLayout);
-        // setting up koloda (for the card swipes) - READ MORE HERE: https://github.com/Yalantis/Koloda-Android
-        koloda = rootView.findViewById(R.id.koloda);
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        adapter = new SwipeAdapter(getContext(), events, fragmentManager);
-        koloda.setAdapter(adapter);
+        vh = new ViewHolder(rootView);
+        currentEventIndex = 0;
+        // fetching event data
+        DatabaseService databaseService = new DatabaseService();
+        databaseService.getAllEvents().thenAccept(events1 -> {
+            events = events1;
+            adapter = new SwipeAdapter(getContext(), events);
+            vh.koloda.setAdapter(adapter);
+        });
+
+        buttonListeners();
 
         /*
           Koloda interface listener functions, don't need to use ALL
          */
-        koloda.setKolodaListener(new KolodaListener() {
+        vh.koloda.setKolodaListener(new KolodaListener() {
             @Override
             public void onNewTopCard(int i) {
             }
@@ -64,12 +85,14 @@ public class FragmentHome extends Fragment {
 
             @Override
             public void onCardSwipedLeft(int i) {
-                handleSwipe(false, currentEventIndex);
+                Toast.makeText(getContext(), "Left Swipe", Toast.LENGTH_SHORT).show();
+                currentEventIndex++;
             }
 
             @Override
             public void onCardSwipedRight(int i) {
-               handleSwipe(true, currentEventIndex);
+                Toast.makeText(getContext(), "Right Swipe", Toast.LENGTH_SHORT).show();
+                currentEventIndex++;
             }
 
             @Override
@@ -80,181 +103,59 @@ public class FragmentHome extends Fragment {
             public void onClickLeft(int i) {
             }
 
-            //TODO DETAILS PAGE (A2) -> show details of event from dialog
+
             @Override
             public void onCardSingleTap(int i) {
-
+                //TODO DETAILS PAGE (A2) -> show details of event from dialog
+                Toast.makeText(getContext(), "Open detailed view", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onCardDoubleTap(int i) {
-
             }
 
             @Override
             public void onCardLongPress(int i) {
-
             }
 
             @Override
             public void onEmptyDeck() { // events finished - need method of refreshing!
-                if (currentEventIndex == events.size()-1) {
-                    emptyEventsLayout.setVisibility(View.VISIBLE);
-                    koloda.setVisibility(View.GONE);
-                    buttonContainer.setVisibility(View.GONE);
-                    currentEventIndex = 0;
-                }
+                vh.emptyEventsLayout.setVisibility(View.VISIBLE);
+                vh.koloda.setVisibility(View.GONE);
+                vh.buttonContainer.setVisibility(View.GONE);
             }
-        });
-
-        // fetching event data
-        fetchEventData();
-        fetchDisinterestedEventData();
-
-        // Add a listener to the events Firestore collection to receive real time updates
-        EventsFirestoreManager.getInstance().addEventsListener((snapshots, e) -> {
-
-            if (e != null) {
-                Log.w(TAG, "Listen failed.", e);
-                return;
-            }
-
-            if (snapshots != null) {
-                for (DocumentChange dc : snapshots.getDocumentChanges()) {
-                    switch (dc.getType()) {
-                        case ADDED:
-                            // if a document was added, add it to events list
-                            Event event = dc.getDocument().toObject(Event.class);
-                            events.add(event);
-                            // update home page?
-                            break;
-                        case MODIFIED:
-                            // TO DO (currently no way to modify events in app)
-                            break;
-                        case REMOVED:
-                            // TO DO (currently no way to delete events in app)
-                            break;
-                    }
-                }
-            }
-        });
-
-        // Set up button click listeners
-        Button nopeButton = rootView.findViewById(R.id.nopeButton);
-        Button interestedButton = rootView.findViewById(R.id.interestedButton);
-        Button refreshButton = rootView.findViewById(R.id.refreshButton);
-
-        // NOT INTERESTED
-        nopeButton.setOnClickListener(v -> {
-            koloda.onButtonClick(false);
-            handleSwipe(false, currentEventIndex);
-        });
-
-        // INTERESTED
-        interestedButton.setOnClickListener(view -> {
-            koloda.onButtonClick(true);
-            handleSwipe(true, currentEventIndex);
-        });
-
-        // REFRESH PAGE
-        refreshButton.setOnClickListener(view -> {
-            emptyEventsLayout.setVisibility(View.GONE);
-            koloda.setVisibility(View.VISIBLE);
-            buttonContainer.setVisibility(View.VISIBLE);
-            fetchEventData(); // fetch data again
-            koloda.reloadAdapterData();
         });
 
         return rootView;
     }
 
-    /**
-     * Fetches the interested event data from the event collection in DB
-     * COULD store this function in another class (DatabaseService class for SRP)
-     */
-    private void fetchEventData() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("events").get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                events.clear();
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Event event = document.toObject(Event.class);
-                        events.add(event);
-                }
-                // Re-notify the adapter when the event data changed
-                adapter.notifyDataSetChanged();
-            } else {
-                Log.e("Database error", "Fetching of events not working properly");
-            }
-        });
-    }
-
-    /**
-     * Fetches the disinterested event data from the event collection in DB
-     * COULD store this function in another class (DatabaseService class for SRP)
-     */
-    private void fetchDisinterestedEventData() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("users").get().addOnCompleteListener(task -> {    // is this the correct collection path?
-            if (task.isSuccessful()) {
-                disinterestedEvents.clear();
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Event event = document.toObject(Event.class);
-                    disinterestedEvents.add(event);
-                }
-                // Re-notify the adapter when the event data changed
-                adapter.notifyDataSetChanged();
-            } else {
-                Log.e("Database error", "Fetching of disinterested events not working properly");
-            }
-        });
-    }
-
-    /**
-     * Checks whether an event is in the disinterested events list
-     */
-    private boolean eventIsDisinterested(Event event) {
-        if (disinterestedEvents.contains(event)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    /**
-     *
-     * @param isInterested - if the user is interested or not
-     * @param index - the particular event index
-     */
-    private void handleSwipe(boolean isInterested, int index) {
-        currentEventIndex = index;
-        if (isInterested) {
-            FireBaseUserDataManager.getInstance().addInterestedEvent(events.get(currentEventIndex));
-            FireBaseUserDataManager.getInstance().getEvents(true);
-        } else {
+    private void buttonListeners() {
+        // NOT INTERESTED
+        vh.nopeButton.setOnClickListener(v -> {
+            vh.koloda.onButtonClick(false);
             FireBaseUserDataManager.getInstance().addDisinterestedEvent(events.get(currentEventIndex));
-            FireBaseUserDataManager.getInstance().getEvents(true);
-            FireBaseUserDataManager.getInstance().getEvents(false);
-            // disinterested events are removed from the browse stack
-            if (eventIsDisinterested(events.get(currentEventIndex))){
-                events.remove(events.get(currentEventIndex));
-            }
-        }
-        nextEvent();
-        Toast.makeText(getContext(), isInterested ? "Interested" : "Not Interested", Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Loads next event
-     */
-    private void nextEvent() {
-        if (currentEventIndex < events.size() - 1) {
             currentEventIndex++;
-            adapter.notifyDataSetChanged();
-        } else { // if all events run out
-           koloda.getKolodaListener().onEmptyDeck();
-        }
+        });
+
+        // INTERESTED
+        vh.interestedButton.setOnClickListener(view -> {
+            vh.koloda.onButtonClick(true);
+            FireBaseUserDataManager.getInstance().addInterestedEvent(events.get(currentEventIndex));
+            currentEventIndex++;
+        });
+
+        // REFRESH PAGE
+        vh.refreshButton.setOnClickListener(view -> {
+            DatabaseService databaseService = new DatabaseService();
+            databaseService.getAllEvents().thenAccept(events1 -> {
+                events = events1;
+                vh.koloda.reloadAdapterData();
+            });
+            vh.emptyEventsLayout.setVisibility(View.GONE);
+            vh.koloda.setVisibility(View.VISIBLE);
+            vh.buttonContainer.setVisibility(View.VISIBLE);
+            currentEventIndex = 1; //set at 1 as bug where first item is skipped on refresh
+        });
     }
 
     public List<Event> getEvents() {
@@ -265,5 +166,3 @@ public class FragmentHome extends Fragment {
         return currentEventIndex;
     }
 }
-
-
