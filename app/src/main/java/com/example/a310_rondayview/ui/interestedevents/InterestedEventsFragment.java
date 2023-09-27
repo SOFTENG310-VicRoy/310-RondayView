ec01e7ee7c2c075f6385fd4f926ef9ba488a297d
tmur647 <100457787+tmur647@users.noreply.github.com>
2023-09-27 14:38:30 +1300

    add hearts back to interested events page (#95)

--- app/src/main/java/com/example/a310_rondayview/ui/interestedevents/InterestedEventsFragment.java
+++ app/src/main/java/com/example/a310_rondayview/ui/interestedevents/InterestedEventsFragment.java
@@ -62,7 +62,7 @@ public class InterestedEventsFragment extends Fragment {
 
         // populate the recycler view
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        InterestedEventsAdapter interestedEventsAdapter = new InterestedEventsAdapter(getContext(), eventsList,true, fragmentManager);
+        InterestedEventsAdapter interestedEventsAdapter = new InterestedEventsAdapter(getContext(), eventsList,false, fragmentManager);
         vh.interestedRecyclerView.setAdapter(interestedEventsAdapter);
         interestedEventsAdapter.notifyDataSetChanged();
     }
